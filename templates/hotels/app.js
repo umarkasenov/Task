0b1461920241bd14d5b3916f0344ae6e9002063b
umarkasenov <casenov2101@icloud.com>
2024-02-23 01:16:44 +0600

    css

---
+++ templates/hotels/app.js
@@ -0,0 +1,10 @@
+VANTA.NET({
+  el: "intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
